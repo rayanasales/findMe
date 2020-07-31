859d6bba659c5d6c665adb4eb545b8e9aea2d153
Rayana Sales <rayanaasales@gmail.com>
2020-07-31 18:26:18 -0300

    fixing folders and components

--- find-me/src/index.js
+++ find-me/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './js/App';
+import './assets/css/index.css';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
