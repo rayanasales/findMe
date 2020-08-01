28d9c4e6a17aa911897baedbc17b7627ebd47b45
Rayana Sales <rayanaasales@gmail.com>
2020-08-01 00:57:38 -0300

    cleaning code. Preparing for receive map and cards

--- find-me/src/components/App.js
+++ find-me/src/components/App.js
@@ -12,54 +12,42 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.loadPlaces();
-  }
-
-  loadPlaces() {
     Api.nearbySearch(-8.05428, -34.8813, 2 * 1000, "gym").then((dataJson) => {
-      debugger;
       return dataJson.json().then((data) => {
-        var dataFormated = [];
-        debugger;
-        data.results.forEach(d => {
-          dataFormated.push({
-            name: d.name,
-            icon: d.icon,
-            rating: d.rating,
-            types: d.types[0]
-          });
-        });
-
-        this.setState({ nearbyPlaces: dataFormated })
+        this.formatData(data);
       })
     }).catch(error => {
-      debugger;
       console.log(error);
     });
-    debugger;
+  }
+
+  formatData(data) {
+    var dataFormated = [];
+
+    data.results.forEach(d => {
+      dataFormated.push({
+        name: d.name,
+        icon: d.icon,
+        rating: d.rating,
+        types: d.types[0]
+      });
+    });
+
+    this.setState({ nearbyPlaces: dataFormated })
   }
 
   render() {
     const { nearbyPlaces } = this.state;
 
-    if (nearbyPlaces.length === 0) {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <span>carregando...</span>
-          </header>
-        </div>
+    var content = nearbyPlaces.length === 0 ? <span>carregando...</span> :
+      nearbyPlaces.map((data, key) =>
+        <div key={key}>{data.name}</div>
       );
-    }
 
     return (
       <div className="App">
         <header className="App-header">
-          {
-            nearbyPlaces.map((data, key) =>
-              <div key={key}>{data.name}</div>
-            )
-          }
+          {content}
         </header>
       </div>
     );
--- find-me/src/util/api.js
+++ find-me/src/util/api.js
@@ -3,8 +3,7 @@ const API_KEY = "AIzaSyBTo1qr8ftqlqSkLhVg_rdHpKbslJxdMas";
 const BASE_GOOGLE_PLACES_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
 
 function nearbySearch(lat, lng, radius, type) {
-
-    debugger;
+    
     var URL = BASE_GOOGLE_PLACES_URL +
         "?location=" + lat + "," + lng +
         "&radius=" + radius +
@@ -12,27 +11,6 @@ function nearbySearch(lat, lng, radius, type) {
         "&key=" + API_KEY;
 
     return fetch(proxyUrl + URL);
-
-    // nearbysearch.then((dataJson) => {
-    //     debugger;
-    //     return dataJson.json().then((data) => {
-    //         var dataFormated = [];
-    //         debugger;
-    //         data.results.forEach(d => {
-    //             dataFormated.push({
-    //                 name: d.name,
-    //                 icon: d.icon,
-    //                 rating: d.rating,
-    //                 types: d.types[0]
-    //             });
-    //         });
-
-    //         return dataFormated;
-    //     })
-    // }).catch(error => {
-    //     debugger;
-    //     console.log(error);
-    // })
 }
 
 const Api = { nearbySearch };
