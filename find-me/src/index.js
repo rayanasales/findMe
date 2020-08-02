3aac613207d3e6b39e033c3c51df00a326b23f66
Rayana Sales <rayanaasales@gmail.com>
2020-08-02 11:16:22 -0300

    routing

--- find-me/src/index.js
+++ find-me/src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Login from './components/Login';
 import './assets/css/Index.css';
+import { BrowserRouter as Router, Switch, Route /*, Link */ } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">App</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav> */}
+
+        {/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <App />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
