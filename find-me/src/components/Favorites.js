74ef239921fccac19c431da1748ef7671d741018
Rayana Sales <rayanaasales@gmail.com>
2020-08-05 17:39:03 -0300

    fixing favorites component

--- find-me/src/components/Favorites.js
+++ find-me/src/components/Favorites.js
@@ -6,6 +6,7 @@ import likeIcon from '../assets/images/like.png';
 import MenuAppBar from "./MenuAppBar";
 import { getSession } from "./../util/storage/auth";
 import { like, dislike, getFavorites } from "./../util/storage/places";
+import GoogleMap from "./GoogleMap";
 
 class ListPlaces extends React.Component {
 
@@ -35,33 +36,36 @@ class ListPlaces extends React.Component {
         const places = getFavorites();
 
         return (
-            <div>
+            <div className="app">
                 <MenuAppBar />
-                <div className="favorites-places-content" style={{ marginTop: "80px" }}>
-                    {
-                        places.map((p, key) =>
-                            <div key={key} className="place-card">
-                                <div className="place-info">
-                                    {
-                                        !p.isLiked ? <img className="like-icon" alt="icon" src={dislikeIcon} onClick={() => { this.likePlace(p) }} /> :
-                                            <img className="like-icon" alt="icon" src={likeIcon} onClick={() => { this.dislikePlace(p) }} />
-                                    }
-                                    <h3 className="place-name label" title={p.name}>{p.name}</h3>
-                                    <div className="place-vicinity" title={p.vicinity}>
-                                        <span>{strings.address}</span>
-                                        {p.vicinity}
+                <div className="content">
+                    <div className="places-content">
+                        {
+                            places.map((p, key) =>
+                                <div key={key} className="place-card">
+                                    <div className="place-info">
+                                        {
+                                            !p.isLiked ? <img className="like-icon" alt="icon" src={dislikeIcon} onClick={() => { this.likePlace(p) }} /> :
+                                                <img className="like-icon" alt="icon" src={likeIcon} onClick={() => { this.dislikePlace(p) }} />
+                                        }
+                                        <h3 className="place-name label" title={p.name}>{p.name}</h3>
+                                        <div className="place-vicinity" title={p.vicinity}>
+                                            <span>{strings.address}</span>
+                                            {p.vicinity}
+                                        </div>
+                                        {
+                                            p.rating ? <div className="place-rating label" title={p.rating}>
+                                                <span>{strings.popular_rating}</span>
+                                                {p.rating}
+                                            </div> : null
+                                        }
+                                        <div className="place-type label" title={p.tags}>{p.tags}</div>
                                     </div>
-                                    {
-                                        p.rating ? <div className="place-rating label" title={p.rating}>
-                                            <span>{strings.popular_rating}</span>
-                                            {p.rating}
-                                        </div> : null
-                                    }
-                                    <div className="place-type label" title={p.tags}>{p.tags}</div>
                                 </div>
-                            </div>
-                        )
-                    }
+                            )
+                        }
+                    </div>
+                    <GoogleMap markers={places} />
                 </div>
             </div>
         );
